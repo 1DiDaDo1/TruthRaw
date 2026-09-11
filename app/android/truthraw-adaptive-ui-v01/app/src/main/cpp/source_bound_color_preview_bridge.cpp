a84186969b9c767c6e291d9697490e63867b7b82
1DiDaDo1 <33970453+1DiDaDo1@users.noreply.github.com>
2026-09-12 00:47:11 +0200

    Use finalized Scientific Preview v0.2 in Android JNI

--- app/android/truthraw-adaptive-ui-v01/app/src/main/cpp/source_bound_color_preview_bridge.cpp
+++ app/android/truthraw-adaptive-ui-v01/app/src/main/cpp/source_bound_color_preview_bridge.cpp
@@ -2,9 +2,9 @@
 
 #include "bounded_srgb_preview_sink_v0_1.h"
 #include "dng_color_binding_producer_v0_2.h"
-#include "finalized_scientific_preview_release_v0_1.h"
+#include "finalized_scientific_preview_release_v0_2.h"
 #include "full_frame_streaming_v0_1.h"
-#include "scientific_master_streaming_binding_v0_1.h"
+#include "scientific_master_streaming_binding_v0_2.h"
 #include "scientific_preview_source_binding_v0_1.h"
 #include "scientific_preview_source_binding_v0_2.h"
 #include "technical_backplane_v0_1.h"
@@ -23,8 +23,8 @@ namespace {
 using truthraw::NeutralReferenceAppearance;
 using truthraw::ResearchEdgeAwareMeasuredPreservingReconstruction;
 using truthraw::dng_color_binding_producer_v0_2::ProducerResult;
-using truthraw::finalized_scientific_preview_release::v0_1::PreviewAuthority;
-using truthraw::finalized_scientific_preview_release::v0_1::ReleaseResult;
+using truthraw::finalized_scientific_preview_release::v0_2::PreviewAuthority;
+using truthraw::finalized_scientific_preview_release::v0_2::ReleaseResult;
 using truthraw::preview_surface_v0_1::BoundedSrgbPreviewSink;
 using truthraw::scientific_preview_binding_v0_1::SourceSeal;
 using truthraw::scientific_preview_binding_v0_2::PreparedScientificPreviewSource;
@@ -74,7 +74,7 @@ jint stream_status(const truthraw::streaming_v0_1::StreamStatus& status) {
     return 4000 + static_cast<jint>(status.code);
 }
 
-jint finalized_status(const truthraw::finalized_scientific_preview_release::v0_1::Status& status) {
+jint finalized_status(const truthraw::finalized_scientific_preview_release::v0_2::Status& status) {
     return 5000 + static_cast<jint>(status.code);
 }
 
@@ -258,7 +258,7 @@ Java_com_truthraw_adaptiveui_NativeTilePreviewBridge_buildFinalizedScientificCol
     auto appearance = std::make_shared<NeutralReferenceAppearance>();
     BoundedSrgbPreviewSink sink(maxEdge);
 
-    truthraw::scientific_master_streaming_binding::v0_1::Options scientificOptions;
+    truthraw::scientific_master_streaming_binding::v0_2::Options scientificOptions;
     scientificOptions.memoryBudgetBytes = static_cast<std::size_t>(maxLogicalResidentBytes);
 
     std::array<truthraw::technical_backplane::v0_1::RoomStatus,
@@ -267,7 +267,7 @@ Java_com_truthraw_adaptiveui_NativeTilePreviewBridge_buildFinalizedScientificCol
 
     ReleaseResult release;
     const auto released =
-        truthraw::finalized_scientific_preview_release::v0_1::create_and_release_finalized_scientific_preview(
+        truthraw::finalized_scientific_preview_release::v0_2::create_and_release_finalized_scientific_preview(
             prepared,
             *source,
             reconstruction,
