aa42192280c0862350fda0ede9723ac70e7bb374
1DiDaDo1 <33970453+1DiDaDo1@users.noreply.github.com>
2026-09-13 22:06:35 +0200

    Version Android app for RGB LinearRaw recovery

--- app/android/truthraw-adaptive-ui-v01/app/build.gradle.kts
+++ app/android/truthraw-adaptive-ui-v01/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.truthraw.adaptiveui"
         minSdk = 31
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.5-raw-dng-export"
+        versionCode = 6
+        versionName = "0.6-rgb-linearraw-restore"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
