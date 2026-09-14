d0047fdff18f32e2aab29b92906eefd4f2ee5919
1DiDaDo1 <33970453+1DiDaDo1@users.noreply.github.com>
2026-09-14 21:05:33 +0200

    Add FotoGraaf Gradle settings

---
+++ app/android/truthraw-fotograaf-honor-v03/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TruthRawFotoGraafHonorV03"
+include(":app")
