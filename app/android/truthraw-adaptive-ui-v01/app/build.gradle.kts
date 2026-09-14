545204d66b1ac1f39c89a04ef6f5bd733938d88e
1DiDaDo1 <33970453+1DiDaDo1@users.noreply.github.com>
2026-09-14 22:29:58 +0200

    Version Main plus FotoGraaf camera v0.2

--- app/android/truthraw-adaptive-ui-v01/app/build.gradle.kts
+++ app/android/truthraw-adaptive-ui-v01/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.truthraw.adaptiveui"
         minSdk = 31
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.5-linear-dng"
+        versionCode = 6
+        versionName = "0.6-main-plus-fotograaf-camera-v0.2"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
