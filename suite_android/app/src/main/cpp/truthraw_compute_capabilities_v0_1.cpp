2e135c8d6e24324cb16cc55f38e74f092589fc4b
1DiDaDo1 <33970453+1DiDaDo1@users.noreply.github.com>
2026-09-21 01:11:05 +0200

    Expose Android Vulkan extension declarations to capability probe

--- suite_android/app/src/main/cpp/truthraw_compute_capabilities_v0_1.cpp
+++ suite_android/app/src/main/cpp/truthraw_compute_capabilities_v0_1.cpp
@@ -4,6 +4,10 @@
 #include <jni.h>
 #include <sys/auxv.h>
 #include <unistd.h>
+
+#ifndef VK_USE_PLATFORM_ANDROID_KHR
+#define VK_USE_PLATFORM_ANDROID_KHR 1
+#endif
 #include <vulkan/vulkan.h>
 
 #include <algorithm>
