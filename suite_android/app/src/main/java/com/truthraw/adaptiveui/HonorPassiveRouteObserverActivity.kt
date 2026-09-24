5767a9afe7e843a3cb29d9254cf8f5364629298b
1DiDaDo1 <33970453+1DiDaDo1@users.noreply.github.com>
2026-09-24 22:03:25 +0200

    ui: apply D.RAW paper chrome to research interface

--- suite_android/app/src/main/java/com/truthraw/adaptiveui/HonorPassiveRouteObserverActivity.kt
+++ suite_android/app/src/main/java/com/truthraw/adaptiveui/HonorPassiveRouteObserverActivity.kt
@@ -22,6 +22,7 @@ class HonorPassiveRouteObserverActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        DrawVisualTheme.applyWindow(this)
         window.setDecorFitsSystemWindows(false)
         setContentView(buildUi())
         refreshStatus()
