b504fd046245126425cef0d2d9a3bdf01fe2138f
1DiDaDo1 <33970453+1DiDaDo1@users.noreply.github.com>
2026-09-24 02:18:51 +0200

    research(f64): bind truthnegative_export_bridge.cpp to isolated F64 module

--- suite_android/app/src/main/cpp/truthnegative_export_bridge.cpp
+++ suite_android/app/src/main/cpp/truthnegative_export_bridge.cpp
@@ -13,6 +13,7 @@
 #include "technical_backplane_v0_1.h"
 #include "tile_native_dng_source_v0_1.h"
 #include "truthraw/core.h"
+#include "scientific_master_f64_reconstruction_v0_1.h"
 #include "truthraw_sha256_v0_69.h"
 
 #include <algorithm>
@@ -31,7 +32,7 @@
 namespace {
 
 using truthraw::CfaPattern;
-using truthraw::ResearchEdgeAwareMeasuredPreservingReconstructionF64;
+using truthraw::scientific_master_f64_reconstruction_v0_1::ResearchEdgeAwareMeasuredPreservingReconstructionF64;
 using truthraw::TileRect;
 using truthraw::dng_color_binding_producer_v0_2::ProducerResult;
 using truthraw::scientific_preview_binding_v0_1::ColorClaimScope;
