8b0f274407be9bedc1b81e42dc874e3eff1de930
1DiDaDo1 <33970453+1DiDaDo1@users.noreply.github.com>
2026-09-24 22:03:32 +0200

    ui: apply D.RAW paper chrome to research interface

--- suite_android/app/src/main/java/com/truthraw/adaptiveui/PassiveHiresTeleStateTimelineActivity.kt
+++ suite_android/app/src/main/java/com/truthraw/adaptiveui/PassiveHiresTeleStateTimelineActivity.kt
@@ -23,6 +23,7 @@ class PassiveHiresTeleStateTimelineActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        DrawVisualTheme.applyWindow(this)
         window.setDecorFitsSystemWindows(false)
         setContentView(buildUi())
         refreshStatus()
